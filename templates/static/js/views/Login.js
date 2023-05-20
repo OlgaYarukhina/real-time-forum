3dbf5f450f88200acd2f52b99cc58ced4ef5b0d5
Jegor Petsorin <egorpechoring@gmail.com>
2023-05-20 15:19:50 +0300

    fixes

--- templates/static/js/views/Login.js
+++ templates/static/js/views/Login.js
@@ -6,6 +6,31 @@ export default class extends AbstractView {
         this.setTitle("Log in page");
     }
 
+    async getScripts(document) {
+        var scripts = [];
+      
+        // var script1 = document.createElement('script');
+        // script1.src = '/static/js/RegisterCode.js';
+        // scripts.push(script1);
+      
+        // var script2 = document.createElement('script');
+        // script2.src = '/static/js/RegisterStyle.js';
+        // scripts.push(script2);
+      
+        var script3 = document.createElement('script');
+        script3.type = 'module';
+        script3.src = 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js';
+        scripts.push(script3);
+      
+        var script4 = document.createElement('script');
+        script4.setAttribute('nomodule', '');
+        script4.src = 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js';
+        scripts.push(script4);
+      
+        return scripts;
+    }
+
+
     async getHtml() {
         return `
         <div class="wrapper_login">
